cd110af81ea69c557ac826e37bf230ed78ae8296
rachele <rachele.terzoli@gmail.com>
2020-04-02 17:43:42 +0200

    codice corretto

--- main.js
+++ main.js
@@ -8,73 +8,104 @@ $(document).ready(function(){
      prevImg
  );
 
- $(".uno").click(
-   prova
- );
 
- function prova(){
-   var imgActive = $( '.images img.active');
+ $( ".nav i" ).click(function() {
+   var imgActive = $( '.images img.active')
    var ballActive = $(' .nav i.active');
+   // `this` is the DOM element that was clicked
 
-   $('.seconda').removeClass('active');
-   $('.due').removeClass('active');
-   $('.terza').removeClass('active');
-   $('.tre').removeClass('active');
-   $('.quarta').removeClass('active');
-   $('.quattro').removeClass('active');
-   $('.prima').addClass('active');
-   $('.uno').addClass('active');
+   //$('.fa-circle').addClass('active');
+   //$('.img').addClass('active');
 
- }
- $(".due").click(
-   provadue
+   // data una lista di elementi jquery ottenere l'indice dell'elemnto che gli stiamo passando
+   //$(listaPalline).index(pallina)
+   var indicePallinaCliccata = $(".nav i").index(this);
 
- );
- function provadue(){
-   var imgActive = $('.images img.active');
-   var ballActive = $(' .nav i.active');
-   $('.prima').removeClass('active');
-   $('.uno').removeClass('active');
-   $('.terza').removeClass('active');
-   $('.tre').removeClass('active');
-   $('.quarta').removeClass('active');
-   $('.quattro').removeClass('active');
-   $('.seconda').addClass('active');
-   $('.due').addClass('active');
- }
+   console.log(indicePallinaCliccata);
 
- $(".tre").click(
-   provatre
- );
- function provatre(){
-   var imgActive = $( '.images img.active');
-   var ballActive = $(' .nav i.active');
-   $('.prima').removeClass('active');
-    $('.uno').removeClass('active');
-    $('.seconda').removeClass('active');
-     $('.due').removeClass('active');
-     $('.quarta').removeClass('active');
-      $('.quattro').removeClass('active');
-   $('.terza').addClass('active');
-    $('.tre').addClass('active');
- }
+   imgActive.removeClass("active");
+   ballActive.removeClass("active");
 
- $(".quattro").click(
-   provaquattro
- );
- function provaquattro(){
-   var imgActive = $( '.images img.active');
-   var ballActive = $(' .nav i.active');
-   $('.prima').removeClass('active');
-    $('.uno').removeClass('active');
-    $('.seconda').removeClass('active');
-     $('.due').removeClass('active');
-     $('.terza').removeClass('active');
-      $('.tre').removeClass('active');
-   $('.quarta').addClass('active');
-    $('.quattro').addClass('active');
- }
+   // data una lista di elementi ottenere un elemento dato l'indice che gli passiamo
+   // $(listaImmagini).eq(indicePallina)
 
+   var immagineDaMostrare = $(".images img").eq(indicePallinaCliccata);
+
+   $(immagineDaMostrare).addClass("active");
+   $(this).addClass("active");
+
+ });
+ // funzione specifica per ogni tondo
+ //
+ // $(".uno").click(
+ //   prova
+ // );
+ //
+ // function prova(){
+ //   var imgActive = $( '.images img.active');
+ //   var ballActive = $(' .nav i.active');
+ //
+ //   $('.seconda').removeClass('active');
+ //   $('.due').removeClass('active');
+ //   $('.terza').removeClass('active');
+ //   $('.tre').removeClass('active');
+ //   $('.quarta').removeClass('active');
+ //   $('.quattro').removeClass('active');
+ //   $('.prima').addClass('active');
+ //   $('.uno').addClass('active');
+ //
+ // }
+ // $(".due").click(
+ //   provadue
+ //
+ // );
+ // function provadue(){
+ //   var imgActive = $('.images img.active');
+ //   var ballActive = $(' .nav i.active');
+ //   $('.prima').removeClass('active');
+ //   $('.uno').removeClass('active');
+ //   $('.terza').removeClass('active');
+ //   $('.tre').removeClass('active');
+ //   $('.quarta').removeClass('active');
+ //   $('.quattro').removeClass('active');
+ //   $('.seconda').addClass('active');
+ //   $('.due').addClass('active');
+ // }
+
+
+
+ // $(".tre").click(
+ //   provatre
+ // );
+ // function provatre(){
+ //   var imgActive = $( '.images img.active');
+ //   var ballActive = $(' .nav i.active');
+ //   $('.prima').removeClass('active');
+ //    $('.uno').removeClass('active');
+ //    $('.seconda').removeClass('active');
+ //     $('.due').removeClass('active');
+ //     $('.quarta').removeClass('active');
+ //      $('.quattro').removeClass('active');
+ //   $('.terza').addClass('active');
+ //    $('.tre').addClass('active');
+ // }
+ //
+ // $(".quattro").click(
+ //   provaquattro
+ // );
+ // function provaquattro(){
+ //   var imgActive = $( '.images img.active');
+ //   var ballActive = $(' .nav i.active');
+ //   $('.prima').removeClass('active');
+ //    $('.uno').removeClass('active');
+ //    $('.seconda').removeClass('active');
+ //     $('.due').removeClass('active');
+ //     $('.terza').removeClass('active');
+ //      $('.tre').removeClass('active');
+ //   $('.quarta').addClass('active');
+ //    $('.quattro').addClass('active');
+ // }
+ //
 
 
 
